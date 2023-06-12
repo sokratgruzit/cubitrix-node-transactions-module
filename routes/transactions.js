56b1c10fbee6c1e15fb63c9939484a1fc58f6274
jinx <talakhadzemariam@gmail.com>
2023-06-12 15:05:47 +0400

    make transfer and code review

--- routes/transactions.js
+++ routes/transactions.js
@@ -5,37 +5,34 @@ const transactions_controller = require("../controllers/transactions_controller"
 const cookieParser = require("cookie-parser");
 
 router.use(cookieParser());
-router.post("/make_transaction", transactions_controller.make_transaction);
+
 router.post(
-  "/update_transaction_status",
-  transactions_controller.update_transaction_status,
+  "/create_global_option",
+  transactions_controller.create_global_option
 );
-
-router.post("/deposit_transaction", transactions_controller.deposit_transaction);
-
-router.post("/create_global_option", transactions_controller.create_global_option);
 
 router.post("/update_options", transactions_controller.update_options);
 
-router.post("/submit_transaction", transactions_controller.submit_transaction);
-
 router.post(
   "/pending_deposit_transaction",
-  transactions_controller.pending_deposit_transaction,
+  transactions_controller.pending_deposit_transaction
 );
 
 router.post(
   "/coinbase_deposit_transaction",
-  transactions_controller.coinbase_deposit_transaction,
+  transactions_controller.coinbase_deposit_transaction
 );
 
 router.post("/coinbase_webhooks", transactions_controller.coinbase_webhooks);
 router.post(
   "/get_transactions_of_user",
-  transactions_controller.get_transactions_of_user,
+  transactions_controller.get_transactions_of_user
 );
 
-router.post("/get_transaction_by_hash", transactions_controller.get_transaction_by_hash);
-router.post("/deliver_amount_test", transactions_controller.deliver_amount_test);
+router.post(
+  "/get_transaction_by_hash",
+  transactions_controller.get_transaction_by_hash
+);
+router.post("/make_transfer", transactions_controller.make_transfer);
 
 module.exports = router;
